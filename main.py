1c1cfcf799ea569dc32564c70453c87c468771d7
Lolbix69 <rbdproductionz@gmail.com>
2025-02-21 16:02:26 -0600

    Fixed Windows now that we are gonna have pygame ce

--- main.py
+++ main.py
@@ -3,49 +3,44 @@ from button import *
 from window import *
 
 pygame.init()
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((1024,768), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 run = True   
 
 
-exitButton = Button(screen, (screen.get_width() - 50, 0), 50, 45, (255,255,255))
+exitButton = Button(screen, (screen.get_width() - 50, 0), 50, 45, (255,0,0))
 
 newWindow = window(screen, (300, 300), 500, 350)
+newWindow2 = window(screen, (300, 400), 500, 350)
 
 while run:
-    mouse_state = pygame.mouse.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if newWindow != 0 and newWindow.clickable():
-                newWindow.dragged = True
-
-            if exitButton.clickable() and exitButton.mode == 'down':
-                run = False
+    if exitButton.clickable() and pygame.mouse.get_just_released()[0]:
+        run = False
             
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if newWindow != 0:
-                newWindow.dragged = False
-
-            if mouse_state[0] and exitButton.mode == 'up' and exitButton.clickable():
-                run = False
-            elif mouse_state[0] and exitButton.mode == 'up' and newWindow != 0 and newWindow.exit.clickable():
-                newWindow = 0
-    
-    if newWindow != 0 and newWindow.dragged:
-            newWindow.pos = pygame.mouse.get_pos()
-
     screen.fill((0,0,0))
     pygame.display.set_caption(str(clock.get_fps()))
     clock.tick(60)
 
-    pygame.draw.rect(screen, (200,200,200), pygame.Rect(0, 0, screen.get_width(), 45))
-
-    if newWindow != 0:
+    if newWindow.run:
+        pygame.draw.rect(newWindow.surface, (0,0,0), pygame.Rect(100,100,50,50))
+        newWindow.screen.blit(newWindow.surface, newWindow.pos)
         newWindow.draw()
+    
+    if newWindow2.run:
+        pygame.draw.rect(newWindow2.surface, (0,0,0), pygame.Rect(100,200,50,50))
+        newWindow2.screen.blit(newWindow2.surface, newWindow2.pos)
+        newWindow2.draw()
 
+
+    #top bar and exit button
+    pygame.draw.rect(screen, (200,200,200), pygame.Rect(0, 0, screen.get_width(), 45))
     exitButton.draw()
+
+
+
 
     pygame.display.flip()
     
--- window.py
+++ window.py
@@ -12,15 +12,30 @@ class window():
         self.window_topper = pygame.image.load("resources/window_topper.png")
         self.window_topper = pygame.transform.scale_by(self.window_topper, 3.90625)
         self.dragged = False
+        self.clicked_dif = pygame.Vector2(0, 0)
+        self.run = True
+
+
     
     def draw(self):
-        self.surface.fill((250,250,250))
-        self.screen.blit(self.surface, self.pos)
-        self.exit.pos = (self.pos[0] + self.width - 31, self.pos[1])
-        self.exit.draw()
-        self.screen.blit(self.window_topper, self.pos)
-    
-    def clickable(self):
-       if pygame.mouse.get_pos()[0] >= self.pos[0] and pygame.mouse.get_pos()[0] <= self.pos[0] + self.width:
-           if pygame.mouse.get_pos()[1] >= self.pos[1] and pygame.mouse.get_pos()[1] <= self.pos[1] + self.height:
-               return True
+        if self.run:
+            self.surface.fill((250,250,250))
+            self.exit.pos = (self.pos[0] + self.width - 31, self.pos[1])
+            self.exit.draw()
+            self.screen.blit(self.window_topper, self.pos)
+
+        #window exit button stuff
+        if self.exit.clickable() and pygame.mouse.get_just_released()[0]:
+            self.run = False
+        
+        #window drag stuff
+        if pygame.mouse.get_pos()[0] >= self.pos[0] and pygame.mouse.get_pos()[0] <= self.pos[0] + self.width:
+           if pygame.mouse.get_pos()[1] >= self.pos[1] and pygame.mouse.get_pos()[1] <= self.pos[1] + 31:
+                if pygame.mouse.get_just_pressed()[0]:
+                    self.dragged = True
+                    self.clicked_dif = pygame.Vector2(pygame.mouse.get_pos()) - self.pos
+        if pygame.mouse.get_just_released()[0] and self.dragged == True:
+            self.dragged = False
+        
+        if self.dragged == True:
+            self.pos = pygame.Vector2(pygame.mouse.get_pos()) + -self.clicked_dif
